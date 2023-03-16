70e6295c134ef03632ba134118237654aee10e94
gerbsec <gerbsec@proton.me>
2023-03-15 18:47:21 -0700

    yesy

--- sumn.c
+++ sumn.c
@@ -3,41 +3,32 @@
 #include <string.h>
 #include <unistd.h>
 
-static int get_process_name(char* pid, char* buffer)
+static int get_process_name(char *pid, char *buffer)
 {
-    FILE    *f;
-    long    numbytes;
-    if(strspn(pid, "0123456789") != strlen(pid)) {
+    long length;
+    if (strspn(pid, "0123456789") != strlen(pid))
+    {
         return 0;
     }
 
     char tmp[255];
     snprintf(tmp, sizeof(tmp), "/proc/%s/cmdline", pid);
-    f = fopen(tmp, "rb");
-    if( f== NULL){
-	    return 1;
+    FILE *f = fopen(tmp, "rb");
+    puts(tmp);
+    if (f)
+    {
+        fscanf(tmp, sizeof(buffer), buffer);
     }
-    fseek(f, 0L, SEEK_END);
-    numbytes = ftell(f);
-    fseek(f, 0L, SEEK_SET);
-
-    buffer = (char*)calloc(numbytes, sizeof(char));
-    if(buffer == NULL){
-        return 1;
+    if (buffer){
+        printf("%s", buffer);
     }
-
-    fread(buffer, sizeof(char), numbytes, f);
-    fclose(f);
-
-    printf(buffer);
     return 0;
-
 }
 
-int main(){
-	char process_name[256];
-	get_process_name("25195", process_name);
-	puts(process_name);
-	return 0;
+int main()
+{
+    char process_name[1000];
+    get_process_name("3430", process_name);
+    puts(process_name);
+    return 0;
 }
-
